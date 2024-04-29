995b95b6f4d564f357d5ccba2d6f23307f289b22
ForesterSTI <mamogobokabelo@gmail.com>
2024-04-29 19:29:34 +0200

    Asks user for name of fruit and add to list

--- Day 1/Lists_and_Collections.py
+++ Day 1/Lists_and_Collections.py
@@ -1,2 +1,7 @@
-my_list = ["apple", "orange", "pineapple", "pear"]    
+# Asks user for name of fruit and add to list
+
+my_list = ["apple", "orange", "pineapple", "pear"]  
+fruit = input("Please enter a fruit name")
+my_list.extend(fruit.split() )  
 print(my_list)
+
