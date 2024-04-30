777e1755c7be7299011308aadef9ca2460bd68ae
ForesterSTI <mamogobokabelo@gmail.com>
2024-04-30 11:01:55 +0200

    learned about pandas and data series and data frame

---
+++ Day 2 Data preporation/Pandas.py
@@ -0,0 +1,15 @@
+        
+
+import pandas as pd
+my_dict1 = {
+    'California': 38332521,
+    'Texas': 26448193,
+    'New York': 19651127,
+    'Florida': 19552860,
+    'Illinois': 12882135
+}
+my_series = pd.Series(my_dict1)
+print(my_series.values)
+
+  
+ 
