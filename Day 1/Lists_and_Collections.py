c5890eaf081080e4d7553b0c47c08500af90b9e4
ForesterSTI <mamogobokabelo@gmail.com>
2024-04-30 08:36:26 +0200

    commented out

--- Day 1/Lists_and_Collections.py
+++ Day 1/Lists_and_Collections.py
@@ -1,7 +1,18 @@
 # Asks user for name of fruit and add to list then prints it
 
 my_list = ["apple", "orange", "pineapple", "pear"]  
+'''
 fruit = input("Please enter a fruit name")
 my_list.extend(fruit.split() )  
 print(my_list)
-
+'''
+Person = {'name': 'John', 
+          'surname':'Smith',
+          'Age':18,
+          'Address':{'Number':123,
+                     'street': "Oak",
+                     'Suburb':"Apprilia",
+                     "City":"Swollow Ville"},
+        
+}
+print(Person.values()) 
